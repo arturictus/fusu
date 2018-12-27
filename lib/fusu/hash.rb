0df76ff4ffbfc4c29b5496726a7ace8a0f0e49c4
Artur Panach <arturictus@gmail.com>
2018-12-27 19:35:14 +0800

    added configurable

--- lib/fusu/hash.rb
+++ lib/fusu/hash.rb
@@ -12,5 +12,9 @@ module Fusu
     def extractable_options?(elem)
       elem.instance_of?(::Hash)
     end
+
+    def reject_key(hash, *keys)
+      hash.reject{ |k, v| keys.include?(k) }
+    end
   end
 end
